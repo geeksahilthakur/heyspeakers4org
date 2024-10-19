52fae8d4c83b6bb24ac622394d03dd6c1a4df48f
Sahil Thakur <94735251+geeksahilthakur@users.noreply.github.com>
2024-10-19 23:00:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAf6os8jnKXLsSIPYulri9H0Wehv_QtmZY",
-    authDomain: "heyspeakers.firebaseapp.com",
-    projectId: "heyspeakers",
-    storageBucket: "heyspeakers.appspot.com",
-    messagingSenderId: "100289054827",
-    appId: "1:100289054827:web:483ecbe9353911d94e892e"
+
 };
 
 firebase.initializeApp(firebaseConfig);
